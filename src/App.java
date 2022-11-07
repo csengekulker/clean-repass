501449401ea6743e5abe4d25fe95a36ec1a4e776
csengekulker <k71608883429@ktch.hu>
2022-11-07 11:20:30 +0100

    refactor: Store methods & indentations

--- src/App.java
+++ src/App.java
@@ -13,28 +13,10 @@ public class App {
     System.out.println("Verzió: 0.0.2");
   }
 
-  public static void tryWriteFile(
-    Store storage
-  ) throws IOException {
 
-    // TODO: filewrite nicer, no reset pass.txt 
-
-    PrintWriter printWriter = new PrintWriter(new FileWriter("pass.txt"));
-
-    printWriter.print(storage.user);
-    printWriter.print(" | ");
-    printWriter.print(storage.place);
-    printWriter.print(" | ");
-
-    if(!storage.hollow()) { 
-      printWriter.print(storage.retrieval()); 
-    }
-    
-    printWriter.close();
-  }
 
   public static Store handleUserInput() {
-    //get and store userinput
+
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Felhasználónév: ");
@@ -53,18 +35,38 @@ public class App {
     return storage;
   }
 
+  public static void tryWriteFile (Store storage) throws IOException {
+
+    // TODO: filewrite nicer, no reset pass.txt 
+    // wannado TODO: write out timestamps (?)
+
+    PrintWriter printWriter = new PrintWriter(new FileWriter("pass.txt"));
+
+    printWriter.print(storage.user);
+    printWriter.print(" | ");
+
+    // TODO: figure out Store methods
+    if(!storage.isEmpty()) { 
+      printWriter.print(storage.retrieval()); 
+    }
+    printWriter.print(storage.place);
+    printWriter.print(" | ");
+    printWriter.close();
+  }
+
   //BIG TODO: clear main() -> own class
   public static void main(String[] args) {
 
     printHeadlines();
 
-    // TODO: reach user input here
     Store storage = handleUserInput();
     
     try {
       tryWriteFile(storage);
+      System.out.println("A fájlbaírás sikeres.");
     } catch (IOException e) {
-        System.err.println("Hiba! A fájlbaírás sikertelen");
+      System.err.println("Hiba! A fájlbaírás sikertelen.");
+      System.err.println(e);
     }
   }
 
--- src/Store.java
+++ src/Store.java
@@ -1,22 +1,32 @@
 public class Store {
-    String user;
-    String pass;
-    String place;
-    public Store(String user, String pass, String place) {
-        this.user = user;
-        this.pass = pass;
-        this.place = place;
+
+  String user;
+  String pass;
+  String place;
+
+  public Store(String user, String pass, String place) {
+    this.user = user;
+    this.pass = pass;
+    this.place = place;
+  }
+
+  public String retrieval() {
+    return pass;
+  }
+  
+  // FIXME: return bool only once
+  // FIXME: replace method name 'hollow'
+
+  //TODO:check other 2 fields aswell
+  public boolean isEmpty() {
+
+    // we assume its not empty
+    boolean empty = false;
+
+    if (this.pass == "") {
+      empty = true;
     }
-    public String retrieval() {
-        return pass;
-    }
-    // FIXME: return bool only once
-    // FIXME: replace method name 'hollow'
-    public boolean hollow() {
-        if(this.pass == "") {
-            return true;
-        }else {
-            return false;
-        }
-    }
+
+    return empty;
+  }
 }

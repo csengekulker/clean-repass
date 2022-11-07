5b6efbf49951911224aa2c9172f0afbf0d926037
csengekulker <k71608883429@ktch.hu>
2022-11-07 10:49:29 +0100

    refactor: trycatch block

--- src/App.java
+++ src/App.java
@@ -4,6 +4,35 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class App {
+
+  public static void printHeadlines() {
+    System.out.println("Balogh Csenge | Szoft_II_N | 2022.11.07");
+
+    System.out.println("Jelszavak tárolása - repass (refactor)");
+
+    System.out.println("Verzió: 0.0.2");
+  }
+
+  public static void tryWriteFile(
+    String user, String pass, String place
+  ) throws IOException {
+
+    // FIXME: proper name for Store object
+    // TODO: filewrite nicer, no reset pass.txt 
+    Store passList = new Store(user, pass, place);
+    FileWriter f = new FileWriter("pass.txt");
+    PrintWriter pwr = new PrintWriter(f);
+    pwr.print(passList.user);
+    if(!passList.hollow()) { pwr.print(passList.retrieval()); }
+    pwr.print(passList.place);
+    pwr.close();
+  }
+
+  public void getUserInput() {
+
+  }
+
+  //BIG TODO: clear main() -> own class
   public static void main(String[] args) {
 
     printHeadlines();
@@ -20,41 +49,13 @@ public class App {
     String place = sc.nextLine();
 
     sc.close();
-
-    // FIXME: iSiker boolean legyen (0 false, 1 true)
-    int iSiker = 0;
     
     try {
-        // TODO: kulon metodus: tryWriteFile()
-        /* 
-        A jelszó, a felhasználónév és a 
-        használati helye a passList 
-        objektumban van tárolva            
-        */
-        // FIXME: proper name for Store object
-        // TODO: filewrite nicer, no reset pass.txt 
-        Store passList = new Store(user, pass, place);
-        FileWriter f = new FileWriter("pass.txt");
-        PrintWriter pwr = new PrintWriter(f);
-        pwr.print(passList.user);
-        if(!passList.hollow()) { pwr.print(passList.retrieval()); }
-        pwr.print(passList.place);
-        pwr.close();
-        iSiker = 1;
+      tryWriteFile(user, pass, place);
     } catch (IOException e) {
         System.err.println("Hiba! A fájlbaírás sikertelen. Keresse meg a fejlesztőt.");
     }
-
-    // FIXME: break lines
-    if(iSiker == 1) { System.out.println("Ok. A kiírás sikeres.");  }else {  System.out.println("Hiba! A kiírás sikertelen"); }
-
   }
 
-  public static void printHeadlines() {
-      System.out.println("Balogh Csenge | Szoft_II_N | 2022.11.07");
 
-      System.out.println("Jelszavak tárolása - repass (refactor)");
-
-      System.out.println("Verzió: 0.0.2");
-  }
 }
